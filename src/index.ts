d0a539f1c6797e84ce6ff1f7b27a54cfbb16bd8d
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-05-21 11:18:14 -0400

    se agrega observables

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import { Observable } from 'rxjs'
 
+// Los observable es un estandar usar el $ a final
+// const obs$ = Observable.create();
 
-console.log('Hola Mundo!');
+// Esto se recomienda no tener un tipo unknow(desconocido) y poner un tipo
+const obs$ = new Observable<String>(subs => {
+    // Necesita tener un valor
 
+    subs.next('Hola');
+    subs.next('mundo');
 
+    // No va a emitir nada mas
+    subs.complete();
+    // Esto jamas se ejecutara
+    subs.next('holi')
+    subs.next('holi')
+});
 
-
-
-
-
+obs$.subscribe(resp => console.log(resp));
